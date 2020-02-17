d41194d6707e476b2c2cf8ac2440a40c2319efd6
Michael Moser <mmoser@redhat.com>
2020-02-17 08:54:38 +0200

    try to upload the package (failed so far)
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
 import os
-from setuptools import setup
-
+import setuptools 
 
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
         return f.read()
 
-setup(
+setuptools.setup(
     name = "roget", 
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Michael Moser",
     author_email = "moser.michael@gmail.com",
     description = ("API to the Roget thesaurus"),
     license = "BSD",                                                                
     keywords = "natural language processing; thesaurus",
-    url = "http://mosermichael.github.io/cstuff/all/projects/2015/02/26/roget.html",
-    packages=['roget', 'tests'],
+    url = "https://github.com/MoserMichael/roget-thesaurus-parser",
+    packages=setuptools.find_packages(),
     long_description=read('README'),
-    zip_safe=False,
-    include_package_data=True,
+    long_description_content_type='text/markdown',
     classifiers=[
         "Natural Language :: English",
 	"Topic :: Text Processing :: Linguistic",
@@ -27,4 +25,5 @@ setup(
  	"Operating System :: OS Independent",
 	"License :: OSI Approved :: BSD License",
     ],
+    python_requires='>=3.6',
 )

8886659e9e7866b1a42bdc11af376f13e18f56df
Michael Moser <moser.michael@gmail.com>
2021-10-21 07:09:15 +0300

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setuptools.setup(
     name = "RogetThesaurus", 
-    version = "0.0.7",
+    version = "0.0.8",
     author = "Michael Moser",
     author_email = "moser.michael@gmail.com",
     description = ("API to the Roget thesaurus"),
